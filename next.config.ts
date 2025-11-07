5f3cad0c231e7951bdadba2b2d99157ce817382b
ChihTeng Ma <chihtengma416@gmail.com>
2025-11-06 21:01:37 -0500

    Configure Next.js for static export

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
-
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
